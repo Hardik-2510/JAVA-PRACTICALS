14e6665d743ad6b5817024539e35d2cbcadbebe6
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-02-07 18:46:58 +0530

    Practical-3(C) - Exam Result Analisys

--- Practical-3(C).java
+++ Practical-3(C).java
@@ -5,25 +5,40 @@ public class practical8 {
 
      // exam result analysis
 
+        int s1 , s2 , s3 , s4 , s5 ;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter student marks (out of 30): ");
-        int marks = sc.nextInt();
+        s1 = sc.nextInt();
+        s2 = sc.nextInt();
+        s3 = sc.nextInt();
+        s4 = sc.nextInt();
+        s5 = sc.nextInt();
 
-        if (marks >= 12) {
-            char grade;
-            if (marks >= 26) {
-                grade = 'A';
-            } else if (marks >= 21) {
-                grade = 'B';
-            } else if (marks >= 13) {
-                grade = 'C';
-            } else {
-                grade = 'D';
+        float avg = (s1 + s2 + s3 + s4 + s5) / 5 ;
+
+        
+
+        if(s1 < 30 && s2 < 30 && s3 < 30 && s4 < 30 && s5 < 30 ){
+            if (avg >= 26){
+                System.out.println("Congratulations ! You Have Passed With A Grades !");
             }
-            System.out.println("Congratulations!! You are passed with grade " + grade + "!");
-        } else {
+            else if(avg >= 21){
+                System.out.println("Congratulations ! You Have Passed With B Grades !");
+            }
+            else if(avg >= 13){
+                System.out.println("Congratulations ! You Have Passed With C Grades !");
+            }    
+            else if(avg >= 12){
+                System.out.println("Congratulations ! You Have Passed With D Grades !");
+            }
+            else if(avg < 12){
             System.out.println("Alert! You are failed and you have to give makeup tests to clear the subject!");
         }
+    }
+    else{
+        System.out.println("Sorry ! Enter Valid Marks Between 0 - 30");
+    }
         sc.close();
     }
 }
